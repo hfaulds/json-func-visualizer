58fc4568a775c2a9fd55778804f51ebf2a816e56
Hayden Faulds <fauldsh@gmail.com>
2014-08-22 13:48:08 +0100

    dont rely on argument prefixes

--- example_controller.js
+++ example_controller.js
@@ -3,35 +3,4 @@ angular.module('exampleApp', ['jsonFunc'])
     $scope.valid_functions = valid_functions;
     $scope.raw_data_columns = raw_data_columns;
     $scope.target_columns = target_columns;
-
-    $scope.json_from_hash = function(func) {
-      if(func && func.args) {
-        var arg_values = _.map(func.args, function(arg) {
-          if(arg.name.split('_')[0] == 'hash') {
-            return _.object(_.compact(arg.keys), arg.values);
-          } else if(arg.name.split('_')[0] == 'list') {
-            return ['list'].concat(_.compact(arg.values));
-          } else {
-            return arg.value;
-          }
-        })
-        var json = [func.name].concat(arg_values);
-        return JSON.stringify(json);
-      }
-    };
-
-    $scope.remove_blanks = function(values) {
-      return _.compact(values).concat(['']);
-    };
-
-    $scope.remove_blanks_from_hash = function(arg) {
-      var new_values = [];
-      _.each(arg.keys, function(key, index) {
-        if(_.identity(key)) {
-          new_values.push(arg.values[index] || "");
-        }
-      });
-      arg.keys = _.compact(arg.keys).concat(['']);
-      arg.values = new_values;
-    };
   }]);
--- json_func_directive.js
+++ json_func_directive.js
@@ -7,13 +7,13 @@ angular.module('jsonFunc', [])
         valid_functions: '=',
         column: '@',
       },
-      scope: function($scope) {
+      link: function($scope) {
         $scope.json_from_hash = function(func) {
           if(func && func.args) {
             var arg_values = _.map(func.args, function(arg) {
-              if(arg.name.split('_')[0] == 'hash') {
+              if(typeof arg === 'object') {
                 return _.object(_.compact(arg.keys), arg.values);
-              } else if(arg.name.split('_')[0] == 'list') {
+              } else if(arg[0] === 'list') {
                 return ['list'].concat(_.compact(arg.values));
               } else {
                 return arg.value;
@@ -23,8 +23,6 @@ angular.module('jsonFunc', [])
             return JSON.stringify(json);
           }
         };
-      },
-      link: function($scope) {
         $scope.valid_functions = angular.copy($scope.valid_functions);
 
         if($scope.column.persisted_func) {
